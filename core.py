4edad4726074885f6840d9daa76e8fcf13112392
Rex Zhang <rex.zhang@gmail.com>
2020-12-15 17:44:22 +0800

    Update default feedback info

--- core.py
+++ core.py
@@ -15,7 +15,7 @@ DEFAULT_FEEDBACK = [
         'icon': ICON_ERROR,
     },
     {
-        'title': 'alsa support time zone and time shift',
+        'title': 'If you want to change time zone or time shift',
         'subtitle': 'Examples: now +08, now +1d',
         'valid': False,
         'icon': ICON_ERROR,
