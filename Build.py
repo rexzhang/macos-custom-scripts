7f2a54dee757a4b07a7a528e2127c2c0a2deb46f
Rex Zhang <rex.zhang@gmail.com>
2023-02-15 17:15:20 +0800

    Update build script

--- Build.py
+++ Build.py
@@ -6,27 +6,25 @@ from os.path import join
 from zipfile import ZipFile
 
 FILENAME_LIST = (
-    'info.plist',
-
-    'entry_point.py',
-    'core.py',
-
+    "info.plist",
+    "entry_point.py",
+    "core.py",
 )
 
 PATH_LIST = (
-    'workflow',
-    'arrow',
-    'backports',
-    'dateutil',
+    "arrow",
+    "backports",
+    "dateutil",
+    "ualfred",
 )
 
 
 def main():
-    with open('version') as f:
-        version = f.readline().rstrip('\n\r ')
+    with open("version") as f:
+        version = f.readline().rstrip("\n\r ")
 
-    zip_filename = 'time-converter.{}.alfredworkflow'.format(version)
-    z = ZipFile(zip_filename, mode='w')
+    zip_filename = "time-converter.{}.alfredworkflow".format(version)
+    z = ZipFile(zip_filename, mode="w")
 
     for filename in FILENAME_LIST:
         z.write(filename)
@@ -34,13 +32,13 @@ def main():
     for pathname in PATH_LIST:
         for root, dirs, files in walk(pathname):
             for filename in files:
-                if filename.rfind('.pyc') == -1:
+                if filename.rfind(".pyc") == -1:
                     z.write(join(root, filename))
 
     z.close()
 
-    print('Create Alfred workflow({}) finished.'.format(zip_filename))
+    print("Create Alfred workflow({}) finished.".format(zip_filename))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
