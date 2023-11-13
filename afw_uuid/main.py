2d0a7ac0afa1800beb3d8a510f4eb62938d48df6
Rex Zhang <rex.zhang@gmail.com>
2023-11-13 15:53:05 +0800

    Update [uuid]

--- afw_uuid/main.py
+++ afw_uuid/main.py
@@ -1,36 +1,37 @@
 import uuid
+from logging import Logger
 
 from afw_runtime import AFWFuncAbc, AFWResponse
 
 
 class AFWFunc(AFWFuncAbc):
-    _data: uuid.UUID | None = None
+    _uuid: uuid.UUID | None = None
     _tips = False
 
-    def make_responses_from_uuid(self, u: uuid.UUID) -> list[AFWResponse]:
+    def make_responses_from_uuid(self) -> list[AFWResponse]:
         return [
             AFWResponse(
-                title=str(u),
+                title=str(self._uuid),
                 subtitle="The UUID as a 32-character lowercase hexadecimal string with dash.",
                 icon=self.icon_info,
             ),
             AFWResponse(
-                title=u.hex,
+                title=self._uuid.hex,
                 subtitle="The UUID as a 32-character lowercase hexadecimal string.",
                 icon=self.icon_info,
             ),
             AFWResponse(
-                title=str(u.int),
+                title=str(self._uuid.int),
                 subtitle="The UUID as a 128-bit integer.",
                 icon=self.icon_info,
             ),
             AFWResponse(
-                title=str(u.urn),
+                title=str(self._uuid.urn),
                 subtitle="The UUID as a URN as specified in RFC 4122.",
                 icon=self.icon_info,
             ),
             AFWResponse(
-                title=f"{u.version}",
+                title=f"{self._uuid.version}",
                 subtitle="The UUID version number (1 through 5, specified is RFC_4122).",
                 icon=self.icon_info,
                 arg=4,
@@ -80,20 +81,20 @@ class AFWFunc(AFWFuncAbc):
         return u
 
     def _createa_responses(self):
-        if self._data:
-            self.append_responses(self.make_responses_from_uuid(self._data))
+        if self._uuid:
+            self.append_responses(self.make_responses_from_uuid())
 
         if self._tips:
             self.append_tips()
 
     def _process(self) -> None:
-        self._data = self._guess_input()
-        if self._data is None:
+        self._uuid = self._guess_input()
+        if self._uuid is None:
             self._tips = True
 
         self._createa_responses()
         return
 
 
-def main(args: list[str], logger) -> list[AFWResponse]:
+def main(args: list[str], logger: Logger) -> list[dict]:
     return AFWFunc(args, logger)()
