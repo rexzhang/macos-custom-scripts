ed372e12ddccafa3cb1edeef57117a386bede8d7
Rex Zhang <rex.zhang@gmail.com>
2025-03-03 13:25:46 +0800

    Fix 全角至半角替换时对文字输入的干扰

--- hammerspoon/modules/banjiao.lua
+++ hammerspoon/modules/banjiao.lua
@@ -6,10 +6,10 @@
 
 -- 预定义生效的应用清单
 local app_map = {
-    ["Code"] = true,
-    ["Drafts"] = true,
-    ["Joplin"] = true,
-    ["Sublime Text"] = true
+    ["com.microsoft.VSCode"] = true,
+    ["com.agiletortoise.Drafts-OSX"] = true,
+    ["net.cozic.joplin-desktop"] = true,
+    ["com.sublimetext.4"] = true
 }
 -- 预定义替换映射表
 local replace_map = {
@@ -21,34 +21,23 @@ local replace_map = {
 }
 
 local function keyboardReplaceListener(event)
-    -- print("-----")
-    -- print(hs.inspect.inspect(event:asData()))
-    -- print(event:getButtonState(0))
-    -- print(hs.inspect.inspect(event:getCharacters()))
-    -- print(hs.inspect.inspect(event:getFlags()))
-    -- print(event:getKeyCode())
-    -- print(event:getProperty(hs.eventtap.event.properties.eventSourceUserData))
-    -- print(event:getRawEventData())
-    -- print(event:getTouchDetails())
-    -- print(event:getTouches())
-    -- print(event:getType())
-    -- print(event:getUnicodeString())
-
     -- 只在指定的应用中生效
-    local app_name = hs.application.frontmostApplication():name()
+    local app_bundle_id = hs.application.frontmostApplication():bundleID()
     -- print(app_name)
-    if not app_map[app_name] then
+    if not app_map[app_bundle_id] then
         return
     end
 
     -- 安装替换表执行替换
     local new_char = replace_map[event:getCharacters()]
     if new_char then
-        local delete_event = require("hs.eventtap")
-        delete_event.keyStroke({}, "delete")
         hs.eventtap.keyStrokes(new_char)
+
+        return true -- 阻止原事件
     end
 end
 
-tap = hs.eventtap.new({ hs.eventtap.event.types.keyUp }, keyboardReplaceListener)
+-- keyUp 会看到原始输入,并可能破坏原有输入(比如键入时被选中的文字)
+-- KeyDown 不会显示原始输入,对输入无干扰
+tap = hs.eventtap.new({ hs.eventtap.event.types.keyDown }, keyboardReplaceListener)
 tap:start()
