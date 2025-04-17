1f6ed5b7c1661f6b511ec7fa268b74d2946dce31
Rex Zhang <rex.zhang@gmail.com>
2025-04-17 22:07:26 +0800

    Update 尽量使用 applescript

--- hammerspoon/modules/applemusic.lua
+++ hammerspoon/modules/applemusic.lua
@@ -1,28 +1,35 @@
---- 权限问题: 系统设置 > 隐私与安全 > 辅助功能，勾选 Hammerspoon
----          如果不勾选不会主动触发权限请求
-
--- 1. 监听锁屏事件
+-- 监听锁屏事件
 local lockWatcher = hs.caffeinate.watcher.new(function(eventType)
     if eventType == hs.caffeinate.watcher.screensDidLock then
-        -- 2. 暂停 Apple Music
-        if hs.application.get("Music") then -- 检查应用是否运行
-            hs.applescript.applescript([[
-                tell application "Music" to pause
+        -- 暂停 Apple Music
+        hs.applescript.applescript([[
+tell application "System Events"
+    set isRunning to exists (processes where name is "Music")
+end tell
+
+if isRunning then
+    tell application "Music" to pause
+end if
             ]])
-        end
+        -- end
     end
 end)
 lockWatcher:start()
 
--- 3. 解锁时恢复播放（可选）
+-- 解锁时恢复播放
 local unlockWatcher = hs.caffeinate.watcher.new(function(eventType)
     if eventType == hs.caffeinate.watcher.screensDidUnlock then
         if hs.application.get("Music") then
             hs.applescript.applescript([[
-                tell application "Music" to play
+tell application "System Events"
+    set isRunning to exists (processes where name is "Music")
+end tell
+
+if isRunning then
+    tell application "Music" to play
+end if
             ]])
         end
     end
 end)
 unlockWatcher:start()
-
